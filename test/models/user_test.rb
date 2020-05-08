84212487fb0e53a31452af808c3c6c158b3271e6
sinsoku <sinsoku.listy@gmail.com>
2020-05-09 02:25:49 +0900

    Add a test code to reproduce issue
    ReadOnlyError occurs even though it is in the
    `connected_to(role: :writing)` block.
    
    ```console
    $ bin/rails test
    Running via Spring preloader in process 22117
    Run options: --seed 65401
    
    # Running:
    
    E
    
    Error:
    UserTest#test_should_not_raise_ActiveRecord::ReadOnlyError:
    ActiveRecord::ReadOnlyError: Write query attempted while in readonly mode: INSERT INTO "users" ("created_at", "updated_at") VALUES (?, ?)
        test/models/user_test.rb:9:in `block (4 levels) in <class:UserTest>'
        test/models/user_test.rb:9:in `block (3 levels) in <class:UserTest>'
        test/models/user_test.rb:8:in `block (2 levels) in <class:UserTest>'
        test/models/user_test.rb:5:in `block in <class:UserTest>'
    
    rails test test/models/user_test.rb:4
    
    Finished in 0.149822s, 6.6746 runs/s, 0.0000 assertions/s.
    1 runs, 0 assertions, 0 failures, 1 errors, 0 skips
    ```

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not raise ActiveRecord::ReadOnlyError" do
+    ActiveRecord::Base.connected_to(role: :reading) do
+      User.first
+
+      assert_nothing_raised do
+        ActiveRecord::Base.connected_to(role: :writing) { User.create! }
+      end
+    end
+  end
 end
